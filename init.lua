9adff732f6040b4b70fa891ccf9e4d8771656669
Jerry <jerryq27+dev@gmail.com>
2024-09-17 00:55:06 -0500

    Basic vim settings.

--- init.lua
+++ init.lua
@@ -1,1 +1,13 @@
 -- Starting point for Neovim config
+local options = {
+	number = true,
+	expandtab = true,
+	shiftwidth = 4,
+	tabstop = 4,
+    softtabstop = 4,
+}
+
+for key, value in pairs(options) do
+	vim.opt[key] = value
+end
+
