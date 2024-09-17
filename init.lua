617027ed69dd01147472cd0a5c1b5bfdae673766
Jerry <jerryq27+dev@gmail.com>
2024-09-17 02:10:39 -0500

    Added telescope and treesitter.

--- init.lua
+++ init.lua
@@ -44,13 +44,28 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- add your plugins here
-    { "navarasu/onedark.nvim", name = "onedark", priority = 1000 }
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "navarasu/onedark.nvim", name = "onedark", priority = 1000 },
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   -- install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+})
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+require("nvim-treesitter.configs").setup({
+    ensure_installed = {"lua"},
+    highlight = { enable = true },
+    indent = { enable = true },
 })
 
 require("onedark").setup()
