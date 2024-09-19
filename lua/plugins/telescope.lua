b09985cfededc5bf2bce5fc26b214301ed877175
Jerry <jerryq27+dev@gmail.com>
2024-09-19 15:46:16 -0500

    Replaced Vim select menus with Telescope-like menus.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,29 @@
 -- Project-wide search
-
-local builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fw", ":Telescope workspaces<CR>")
+--  telescope-ui-select - provides telescope-like select menus
 
 return {
-    "nvim-telescope/telescope.nvim", tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {},
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local builtin = require("telescope.builtin")
+
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fw", ":Telescope workspaces<CR>")
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        opts = {
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown({})
+                }
+            },
+        },
+        config = function()
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
