d9ab010f13fcddce49085faed5eb003e823aa5a1
Jerry <jerryq27+dev@gmail.com>
2024-09-19 12:00:03 -0500

    Added plugins to configure LSP support.

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,26 @@
+-- LSP configuration plugins
+--  mason - LSP/Linter/Formatter manager
+--  mason-lspconfig - provides a way to ensure/auto install LSPs
+--  nvim-lspconfig - Sets up the connection between Neovim and the LSP
+
+return {
+    { "williamboman/mason.nvim", opts = {} },
+    { 
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = { "lua_ls" }
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            lspconfig.lua_ls.setup({})
+
+            vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>gt", vim.lsp.buf.definition, {})
+        end
+    },
+}
