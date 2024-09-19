f839217e9fc73b100268fd1392af4c31371a7706
Jerry <jerryq27+dev@gmail.com>
2024-09-19 01:24:24 -0500

    Added a project manager and keymap to search projects.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,8 +4,10 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fw", ":Telescope workspaces<CR>")
 
 return {
     "nvim-telescope/telescope.nvim", tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
 }
---
+++ lua/plugins/workspaces.lua
@@ -0,0 +1,12 @@
+-- Project manager
+
+vim.keymap.set("n", "<leader>wl", ":WorkspacesList<CR>")
+
+return {
+    "natecraddock/workspaces.nvim",
+    opts = {
+        hooks = {
+            open = { ":Neotree" },
+        },
+    },
+}
