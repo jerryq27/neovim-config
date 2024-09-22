b05d146cd7bd884491fef3addbd10f19f008354e
Jerry <jerryq27+dev@gmail.com>
2024-09-22 17:08:07 -0500

    Added none-ls for formatting and linting.

---
+++ lua/plugins/nonels.lua
@@ -0,0 +1,15 @@
+-- Provides and LSP-like communication between Neovim and formatters/linters
+
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+	end,
+}
