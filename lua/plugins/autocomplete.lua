3506f0e46a2c3fbae53d51226d39a2985b41e0c4
Jerry <jerryq27+dev@gmail.com>
2024-09-22 17:08:39 -0500

    Added configuration for autocomplete and snippets.

---
+++ lua/plugins/autocomplete.lua
@@ -0,0 +1,51 @@
+-- Autocompletions and snippets
+--  cmp-nvim-lsp - source of completions/snippets from LSP
+--  LuaSnip - source of snippets
+--  cmp_luasnip - expands out a snippet or completion
+--  friendly-snippets - another source of snippets for many lanugages
+--  nvim-cmp - powers pop-up window that contains snippets/completions
+
+return {
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			-- Set up nvim-cmp.
+			local cmp = require("cmp")
+
+			cmp.setup({
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					-- completion = cmp.config.window.bordered(),
+					-- documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,8 +15,9 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            lspconfig.lua_ls.setup({})
+            lspconfig.lua_ls.setup({ capabilities = capabilities })
 
             vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
