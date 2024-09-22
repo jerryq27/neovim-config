dbe99010cf26b542e051952854bdb3151f0cdac8
Jerry <jerryq27+dev@gmail.com>
2024-09-22 17:06:48 -0500

    Cleanup.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,10 +5,9 @@ local options = {
 	expandtab = true,
 	shiftwidth = 4,
 	tabstop = 4,
-    softtabstop = 4,
+	softtabstop = 4,
 }
 
 for key, value in pairs(options) do
 	vim.opt[key] = value
 end
-
