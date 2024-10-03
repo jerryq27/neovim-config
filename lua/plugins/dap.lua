a3abf131a545d5a27fc7029ed532e3bb48d2b867
Jerry <jerryq27+dev@gmail.com>
2024-10-03 18:21:45 -0500

    Starting on a DAP configuration.

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,41 @@
+-- Debug Adapter Protocol: Config for debuggers
+-- nvim-dap - Allows Neovim to communicate with the adapter protocol
+-- nvim-dap-ui - Provies nice panes and windows for debuggers within Neovim
+-- nvim-nio - asynchronous library for Neovim required by nvim-dap-ui
+return {}
+-- return { "puremourning/vimspector" }
+
+-- return {
+-- 	"mfussenegger/nvim-dap",
+-- 	dependencies = {
+-- 		"rcarriga/nvim-dap-ui",
+-- 		"nvim-neotest/nvim-nio",
+-- 	},
+-- 	opt = function()
+-- 		local dap = require("dap")
+-- 		local dapui = require("dapui")
+--
+--         dapui.setup()
+--
+-- 		dap.listeners.before.attach.dapui_config = function()
+-- 			dapui.open()
+-- 		end
+-- 		dap.listeners.before.launch.dapui_config = function()
+-- 			dapui.open()
+-- 		end
+-- 		dap.listeners.before.event_terminated.dapui_config = function()
+-- 			dapui.close()
+-- 		end
+-- 		dap.listeners.before.event_exited.dapui_config = function()
+-- 			dapui.close()
+-- 		end
+--
+-- 		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+-- 		vim.keymap.set("n", "<Leader>ds", dap.step_over, {})
+-- 		vim.keymap.set("n", "<Leader>di", dap.step_into, {})
+-- 		vim.keymap.set("n", "<Leader>do", dap.step_out, {})
+-- 		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+--
+-- 		return {}
+-- 	end,
+-- }
