000f6bbc2f4dd7ea38497e92caa3c51d308e8401
Jerry <jerryq27+dev@gmail.com>
2024-10-10 22:54:18 -0500

    Added highlight on yank from Kickstart.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,13 +1,14 @@
 -- Vim options
 
 local options = {
-	number = true,
-	expandtab = true,
-	shiftwidth = 4,
-	tabstop = 4,
-	softtabstop = 4,
+    number = true,
+    expandtab = true,
+    shiftwidth = 4,
+    tabstop = 4,
+    softtabstop = 4,
+    cursorline = true,
 }
 
 for key, value in pairs(options) do
-	vim.opt[key] = value
+    vim.opt[key] = value
 end
