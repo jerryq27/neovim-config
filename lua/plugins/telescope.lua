1e1ac294ca43cd367193863c1e3f63c01e283874
Jerry <jerryq27+dev@gmail.com>
2024-11-21 12:09:18 -0600

    Cleaning up configs and using key options.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,31 +2,45 @@
 --  telescope-ui-select - provides telescope-like select menus
 
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = function()
-			local builtin = require("telescope.builtin")
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {},
+        keys = {
+            {
+                "<leader>ff",
+                function()
+                    require("telescope.builtin").find_files()
+                end,
+                desc = "Search files in project"
+            },
+            {
+                "<leader>fg",
+                function()
+                    require("telescope.builtin").live_grep()
+                end,
+                desc = "Search all using grep"
+            },
+            {
+                "<leader>fw",
+                "<CMD>Telescope workspaces<CR>",
+                desc = "Search all workspaces"
+            },
+        }
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        opts = function()
+            require("telescope").load_extension("ui-select")
 
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fw", ":Telescope workspaces<CR>")
-			return {}
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		opts = function()
-			require("telescope").load_extension("ui-select")
-
-			return {
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			}
-		end,
-	},
+            return {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            }
+        end,
+    },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,16 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = { "lua" },
-		highlight = { enable = true },
-		indent = { enable = true },
-	},
+	-- opts = {
+	-- 	ensure_installed = { "lua", "python" },
+	-- 	highlight = { enable = true },
+	-- 	indent = { enable = true },
+	-- },
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            ensure_installed = { "lua", "python", "javascript", "css", "html" },
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
 }
