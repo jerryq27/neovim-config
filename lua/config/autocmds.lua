bed19d6d0054f0a4467a3a08ae5b560ec1d256e0
Jerry <jerryq27+dev@gmail.com>
2024-12-07 13:00:09 -0600

    Created function to map schemes to projects and hooked into VimEnter event.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,23 @@
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+local function determineColorScheme()
+    local workspaces = require "workspaces"
+    local project = workspaces.name()
+    local colorschemes = {
+        nvim = "vscode",
+        spacevibes = "tokyonight-night",
+        carprofiles = "kanagawa-paper",
+    }
+
+    local projectscheme = colorschemes[project]
+    if projectscheme ~= nil then
+        return projectscheme
+    else
+        return "vscode"
+    end
+end
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
@@ -12,25 +29,22 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end,
 })
 
--- Set colorscheme based on parent directory to determine the type of project.
+-- Project based colorschemes
+--  Set colorscheme when switching projects.
 vim.api.nvim_create_autocmd("DirChanged", {
-    desc = "Sets colorscheme based on the type of project",
-    group = vim.api.nvim_create_augroup("jconfig-colorscheme", { clear = true }),
+    desc = "Sets colorscheme when switching projects",
+    group = vim.api.nvim_create_augroup("jconfig-dirchanged-colorscheme", { clear = true }),
     callback = function()
-        -- local current_dir = vim.fn.getcwd()
-        local workspace = require "workspaces"
-        local project = workspace.name()
-        local projectschemes = {
-            nvim = "vscode",
-            spacevibes = "tokyonight-night",
-            carprofiles = "kanagawa-paper",
-        }
-
-        if projectschemes[project] ~= nil then
-            vim.cmd.colorscheme(projectschemes[project])
-        else
-            vim.cmd.colorscheme "vscode"
-        end
+        vim.cmd.colorscheme(determineColorScheme())
+        vim.cmd.hi "Comment gui=none"
+    end,
+})
+--  Set colorscheme when opening Neovim.
+vim.api.nvim_create_autocmd("VimEnter", {
+    desc = "Sets colorscheme when opening Neovim",
+    group = vim.api.nvim_create_augroup("jconfig-vimenter-colorscheme", { clear = true }),
+    callback = function()
+        vim.cmd.colorscheme(determineColorScheme())
         vim.cmd.hi "Comment gui=none"
     end,
 })
